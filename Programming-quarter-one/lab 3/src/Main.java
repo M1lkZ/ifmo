c9e0fdef04a686505d5aeac47a7ef627c8a755ae
M1lkZ <nickolasmarwin@gmail.com>
2022-10-10 00:43:02 +0300

    test

--- Programming-quarter-one/lab 3/src/Main.java
+++ Programming-quarter-one/lab 3/src/Main.java
@@ -1,5 +1,14 @@
+import persons.Kozlik;
+import persons.Neznaika;
+import persons.Person;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Person Notknower = new Neznaika();
+        Person Kozlik = new Kozlik();
+        System.out.println(Notknower.hashCode());
+        System.out.println(Notknower.toString());
+        System.out.println(Notknower.equals(Kozlik));
+        System.out.println(Kozlik.returnName());
     }
 }
